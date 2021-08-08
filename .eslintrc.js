28f061ba22e6853e6cf5b9d1368bbf51b31e2ff6
jobscale <jobscale@users.noreply.github.com>
2021-08-09 05:26:51 +0900

    lint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,13 +17,7 @@ module.exports = {
   rules: {
     indent: ['error', 2, { MemberExpression: 0 }],
     'arrow-parens': 'off',
-    'no-return-assign': 'off',
-    'no-plusplus': 'off',
-    'no-confusing-arrow': 'off',
     'class-methods-use-this': 'off',
-    'import/no-unresolved': 'off',
     'no-await-in-loop': 'off',
-    'no-restricted-syntax': 'off',
-    'lines-between-class-members': 'off',
   },
 };
